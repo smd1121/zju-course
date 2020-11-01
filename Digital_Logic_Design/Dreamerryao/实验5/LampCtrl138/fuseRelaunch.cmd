51ce9430570238fd1045295bef7aecefe9dfda6b
XuanInsr <2087868649@qq,com>
2020-11-01 19:08:12 +0800

    Delete something
    删除了一些不应该放上来的东西）

--- Digital_Logic_Design/Dreamerryao/实验5/LampCtrl138/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "D:/ISEpro/LampCtrl138/LampCtrl_LampCtrl_sch_tb_isim_beh.exe" -prj "D:/ISEpro/LampCtrl138/LampCtrl_LampCtrl_sch_tb_beh.prj" "work.LampCtrl_LampCtrl_sch_tb" "work.glbl" 
